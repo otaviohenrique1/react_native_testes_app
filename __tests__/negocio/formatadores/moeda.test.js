31a173cb2d466e1eec0a13ea2a7b6ce95a84b836
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-02-16 17:39:02 -0300

    commit 4

---
+++ __tests__/negocio/formatadores/moeda.test.js
@@ -0,0 +1,19 @@
+import { formataBrasileiroParaDecimal, formataDecimalParaReal } from "../../../src/negocio/formatadores/moeda";
+
+
+describe("negocio/formatadores/moeda", () => {
+  describe("formataBrasileiroParaDecimal", () => {
+    it("Deve retornar 8.59 quando o valor for '8,59'", () => {
+      const resultado = formataBrasileiroParaDecimal("8,59");
+      expect(resultado).toBe(8.59); /* Oque ele deve receber */
+    });
+  });
+
+  describe("formataDecimalParaReal", () => {
+    it("Deve retornar R$ 8,59 quando o valor for 8.59", () => {
+      const resultado = formataDecimalParaReal(8.59);
+      expect(resultado).toMatch(/R\$\s8,59/); /* toMatch => usa e valida com regex */
+      /* \s => representa qualquer espaço */
+    });
+  });
+});
